9d524e41cdc5116dc7d60ab517f2b532e663a4e8
marcio lucas <lucasmrc2014@gmail.com>
2025-07-22 08:52:44 -0300

    feat: adicionando outras entidades

---
+++ forum-hub/src/main/java/br/com/alura/challenge/forumhub/domain/curso/Curso.java
@@ -0,0 +1,25 @@
+package br.com.alura.challenge.forumhub.domain.curso;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "cursos")
+@Entity(name = "Curso")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String categoria;
+
+}
+
---
+++ forum-hub/src/main/java/br/com/alura/challenge/forumhub/domain/usuario/Usuario.java
@@ -0,0 +1,42 @@
+package br.com.alura.challenge.forumhub.domain.usuario;
+
+import br.com.alura.challenge.forumhub.domain.perfil.Perfil;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet; // Importar HashSet
+import java.util.Set;      // Importar Set
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "usuario_perfis",
+            joinColumns = @JoinColumn(name = "usuario_id"),
+            inverseJoinColumns = @JoinColumn(name = "perfil_id")
+    )
+    private Set<Perfil> perfis = new HashSet<>();
+
+    public Usuario(DadosUsuario dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.senha = dados.senha();
+    }
+}

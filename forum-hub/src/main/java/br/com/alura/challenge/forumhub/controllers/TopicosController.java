930ced667c6b034f4ce774b37ea9d1bf358d1de5
marcio lucas <lucasmrc2014@gmail.com>
2025-07-22 08:54:42 -0300

    feat(controller): Implementa endpoint POST /topicos

--- forum-hub/src/main/java/br/com/alura/challenge/forumhub/controllers/TopicosController.java
+++ forum-hub/src/main/java/br/com/alura/challenge/forumhub/controllers/TopicosController.java
@@ -1,37 +1,32 @@
 package br.com.alura.challenge.forumhub.controllers;
 
-
 import br.com.alura.challenge.forumhub.domain.topico.DadosCadastroTopico;
 import br.com.alura.challenge.forumhub.domain.topico.DetalhamentoDadosTopico;
-import br.com.alura.challenge.forumhub.domain.topico.Topico;
-import br.com.alura.challenge.forumhub.domain.topico.TopicosRepository;
+import br.com.alura.challenge.forumhub.services.TopicoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+
 @RestController
-@RequestMapping("topicos")
+@RequestMapping("/topicos")
 public class TopicosController {
 
     @Autowired
-    private TopicosRepository repository;
-
+    private TopicoService topicoService;
 
     @PostMapping
-    @Transactional
-    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroTopico dados, UriComponentsBuilder uriBuilder) {
-        var topico = new Topico(dados);
-//        repository.save(topico);
+    public ResponseEntity<DetalhamentoDadosTopico> cadastrar(@RequestBody @Valid DadosCadastroTopico dados, UriComponentsBuilder uriBuilder) {
 
-        var uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+        DetalhamentoDadosTopico detalhamentoDto = topicoService.cadastrar(dados);
+        URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(detalhamentoDto.id()).toUri();
 
-        return ResponseEntity.created(uri).body(new DetalhamentoDadosTopico(topico));
+        return ResponseEntity.created(uri).body(detalhamentoDto);
     }
-
 }

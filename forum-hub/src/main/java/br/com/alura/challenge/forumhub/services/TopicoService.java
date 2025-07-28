1f108bb7c4b5e5e60de0beccdd0e2b7f8f36b220
marcio lucas <lucasmrc2014@gmail.com>
2025-07-28 09:07:03 -0300

    feat: logica para atuailizar, detalhar e excluir.

--- forum-hub/src/main/java/br/com/alura/challenge/forumhub/services/TopicoService.java
+++ forum-hub/src/main/java/br/com/alura/challenge/forumhub/services/TopicoService.java
@@ -7,7 +7,7 @@ import br.com.alura.challenge.forumhub.repositories.CursoRepository;
 import br.com.alura.challenge.forumhub.repositories.TopicosRepository;
 import br.com.alura.challenge.forumhub.repositories.UsuarioRepository;
 import jakarta.persistence.EntityNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -16,39 +16,75 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class TopicoService {
 
-    @Autowired
-    private TopicosRepository topicoRepository;
+    private final TopicosRepository topicoRepository;
+    private final UsuarioRepository usuarioRepository;
+    private final CursoRepository cursoRepository;
 
-    @Autowired
-    private UsuarioRepository usuarioRepository;
+    public TopicoService(TopicosRepository topicoRepository, UsuarioRepository usuarioRepository, CursoRepository cursoRepository) {
+        this.topicoRepository = topicoRepository;
+        this.usuarioRepository = usuarioRepository;
+        this.cursoRepository = cursoRepository;
+    }
 
-    @Autowired
-    private CursoRepository cursoRepository;
 
     @Transactional
     public DetalhamentoDadosTopico cadastrar(CadastroDadosTopico dados) {
+        validarDuplicidade(dados.titulo(), dados.mensagem());
 
-        if(topicoRepository.existsByTituloAndMensagem(dados.titulo(), dados.mensagem())) {
-            throw new IllegalStateException("Já existe um tópico com o mesmo título e mensagem.");
-        }
-
-        Usuario autor = usuarioRepository.findById(dados.idAutor())
-                .orElseThrow(() -> new EntityNotFoundException("Autor com id " + dados.idAutor() + " não encontrado."));
-
-        Curso curso = cursoRepository.findById(dados.idCurso())
-                .orElseThrow(() -> new EntityNotFoundException("Curso com id " + dados.idCurso() + " não encontrado."));
-
+        Usuario autor = buscarUsuarioPorId(dados.idAutor());
+        Curso curso = buscarCursoPorId(dados.idCurso());
 
         Topico topico = new Topico(dados.titulo(), dados.mensagem(), autor, curso);
-
         topicoRepository.save(topico);
 
         return new DetalhamentoDadosTopico(topico);
     }
 
     public Page<ListagemDadosTopico> listar(Pageable paginacao) {
-        var pagina = topicoRepository.findAll(paginacao).map(DetalhamentoDadosTopico::new);
-
-        return pagina.map(ListagemDadosTopico::new);
+        return topicoRepository.findAll(paginacao)
+                .map(DetalhamentoDadosTopico::new)
+                .map(ListagemDadosTopico::new);
     }
+
+    @Transactional
+    public DetalhamentoDadosTopico atualizar(AtualizacaoDadosTopico dados) {
+        validarDuplicidade(dados.titulo(), dados.mensagem());
+
+        var topico = topicoRepository.findById(dados.id())
+                .orElseThrow(() -> new EntityNotFoundException("Topico com id não encontrado"));
+        topico.atualizarInformacoes(dados);
+
+        return new DetalhamentoDadosTopico(topico);
+    }
+
+    public DetalhamentoDadosTopico detalhar(Long id) {
+        var topico = topicoRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Tópico com id " + id + " não encontrado."));
+
+        return new DetalhamentoDadosTopico(topico);
+    }
+
+    @Transactional
+    public void excluir(Long id) {
+        topicoRepository.deleteById(id);
+    }
+
+
+    private void validarDuplicidade(String titulo, String mensagem) {
+        if (topicoRepository.existsByTituloAndMensagem(titulo, mensagem)) {
+            throw new IllegalStateException("Já existe um tópico com o mesmo título e mensagem.");
+        }
+    }
+
+    private Usuario buscarUsuarioPorId(Long idAutor) {
+        return usuarioRepository.findById(idAutor)
+                .orElseThrow(() -> new EntityNotFoundException("Autor com id " + idAutor + " não encontrado."));
+    }
+
+    private Curso buscarCursoPorId(Long idCurso) {
+        return cursoRepository.findById(idCurso)
+                .orElseThrow(() -> new EntityNotFoundException("Curso com id " + idCurso + " não encontrado."));
+    }
+
+
 }

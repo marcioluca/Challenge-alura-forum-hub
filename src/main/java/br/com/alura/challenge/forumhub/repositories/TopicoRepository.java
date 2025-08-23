ce830953d5cef917120c0bbc387a091a06ab665f
Marcio Lucas <lucasmrc2014@gmail.com>
2025-08-23 15:28:17 -0300

    fix: corrige nome repository

--- src/main/java/br/com/alura/challenge/forumhub/repositories/TopicoRepository.java
+++ src/main/java/br/com/alura/challenge/forumhub/repositories/TopicoRepository.java
@@ -5,9 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface TopicosRepository extends JpaRepository<Topico, Long> {
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
     boolean existsByTituloAndMensagem(String titulo, String mensagem);
 
     Page<Topico> findAllByAtivoTrue(Pageable paginacao);

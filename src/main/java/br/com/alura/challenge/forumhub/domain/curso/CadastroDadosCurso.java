872e8ac366f4b6db99c1a4f9e239b21ecff5007c
Marcio Lucas <lucasmrc2014@gmail.com>
2025-08-23 15:30:01 -0300

    DTO para cadastro de cursos

---
+++ src/main/java/br/com/alura/challenge/forumhub/domain/curso/CadastroDadosCurso.java
@@ -0,0 +1,6 @@
+package br.com.alura.challenge.forumhub.domain.curso;
+
+public record CadastroDadosCurso(String id,
+                                 String nome,
+                                 String categoria) {
+}

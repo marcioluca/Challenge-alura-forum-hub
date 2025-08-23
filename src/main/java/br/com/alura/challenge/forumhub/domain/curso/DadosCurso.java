e1c3cffd4c60e8f88d7cfbf95731f333e48c7ba8
Marcio Lucas <lucasmrc2014@gmail.com>
2025-08-23 15:28:47 -0300

    DTO para Dados dos cursos

--- src/main/java/br/com/alura/challenge/forumhub/domain/curso/DadosCurso.java
+++ src/main/java/br/com/alura/challenge/forumhub/domain/curso/DadosCurso.java
@@ -3,10 +3,11 @@ package br.com.alura.challenge.forumhub.domain.curso;
 import jakarta.validation.constraints.NotBlank;
 
 public record DadosCurso(
+                         Long id,
                          @NotBlank
                          String nome,
                          String categoria) {
     public DadosCurso(Curso curso) {
-        this(curso.getNome(), curso.getCategoria());
+        this(curso.getId(), curso.getNome(), curso.getCategoria());
     }
 }

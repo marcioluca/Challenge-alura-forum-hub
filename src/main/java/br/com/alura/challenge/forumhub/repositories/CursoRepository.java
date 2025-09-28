45960d5eb27477f703314bcf3ed89e8ef6108d52
Marcio Lucas <lucasmrc2014@gmail.com>
2025-09-28 19:30:24 -0300

    feat: metodo para listar apenas cursos ativos

--- src/main/java/br/com/alura/challenge/forumhub/repositories/CursoRepository.java
+++ src/main/java/br/com/alura/challenge/forumhub/repositories/CursoRepository.java
@@ -1,10 +1,13 @@
 package br.com.alura.challenge.forumhub.repositories;
 
 import br.com.alura.challenge.forumhub.domain.curso.Curso;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface CursoRepository extends JpaRepository<Curso, Long> {
+
+    Page<Curso> findAllByAtivoTrue(Pageable paginacao);
 }
